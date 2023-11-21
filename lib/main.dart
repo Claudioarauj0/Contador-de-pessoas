8bb9828938df36a95b5e8aea86ccb9ae91b1a4d9
Claudioarauj0 <claudinhosas@gmail.com>
2023-11-21 13:10:11 -0300

    alterações

--- lib/main.dart
+++ lib/main.dart
@@ -37,81 +37,54 @@ class _HomePageState extends State<HomePage> {
       count++;
     });
     print(count);
-
   }
 
   bool get isEmpty => count == 0;
   bool get isfull => count == 500;
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/contei.png'), fit: BoxFit.cover)),
+      appBar: AppBar(
+        title: const Text("GERENCIAMENTO"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              isfull ? 'LOTADO' : 'QUANTIDADE',
-              style:  TextStyle(
-                  fontSize: 35,
-                  color: isfull ? Colors.green : Colors.white,
-                  fontWeight: FontWeight.w700),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              '$count',
-              style: TextStyle(
-                fontSize: 100,
-                color: isfull ? Colors.red : Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Data", border: OutlineInputBorder()),
               ),
             ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: isEmpty ? null : decrement,
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor:
-                        isEmpty ? Colors.white.withOpacity(0.3) : Colors.white,
-                    fixedSize: const Size(100, 100),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  child: const Text(
-                    'SAIU',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 20),
-                TextButton(
-                  onPressed: isfull ? null : increment,
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor:
-                        isfull ? Colors.white.withOpacity(0.3) : Colors.white,
-                    fixedSize: const Size(100, 100),  
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  child: const Text(
-                    'ENTROU',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Nome do dirigente",
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Nome do ministro",
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Tema da ministração",
+                    border: OutlineInputBorder()),
+              ),
             ),
           ],
         ),
